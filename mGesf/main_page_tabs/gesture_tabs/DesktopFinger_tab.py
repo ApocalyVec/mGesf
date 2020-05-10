789e1f1fa5189eb7c4259c982eb2065b350d216c
N e n e <makiasagawa@gmail.com>
2020-05-10 17:11:18 -0400

    rollback

--- mGesf/main_page_tabs/gesture_tabs/DesktopFinger_tab.py
+++ mGesf/main_page_tabs/gesture_tabs/DesktopFinger_tab.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout
 
-from utils.gesture_tabs_GUI_utils import setup_operation_tabs
-from utils.main_window_GUI_util import init_container
+from utils.GUI_gesture_tab import setup_operation_tabs
+from utils.GUI_main_window import init_container
 
 
 class DesktopFinger_tab(QWidget):
@@ -9,8 +9,8 @@ class DesktopFinger_tab(QWidget):
         super().__init__()
         self.main_page = QHBoxLayout(self)
 
-        self.operation_block = init_container(parent=self.main_page, vertical=False)
-        setup_operation_tabs(self.operation_block)
+        # self.operation_block = init_container(parent=self.main_page, vertical=False)
+        # setup_operation_tabs(self.operation_block)
 
         self.setLayout(self.main_page)
         self.show()
