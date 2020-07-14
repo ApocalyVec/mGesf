978efed48fb362d434a57fa8a5a73c22e36eee8d
jiananli306 <33046262+jiananli306@users.noreply.github.com>
2020-07-14 18:13:59 -0400

    add XeThru X4M300 radar row data example

--- examples/xeThruTest.py
+++ examples/xeThruTest.py
@@ -2,21 +2,19 @@ from utils.XeThru_utils.xeThruX4_interface import xeThruX4SensorInterface
 from utils.XeThru_utils.xeThruX4_algorithm import *
 import matplotlib.pyplot
 
-
-
 if __name__ == '__main__':
     X4M300 = xeThruX4SensorInterface()
 
-    X4M300.config_x4_sensor('COM8', max_range=0.6, FPS=1)
+    X4M300.config_x4_sensor(device_name='COM8', min_range=-0.1, max_range=0.4,
+                            center_frequency=3, FPS=10, baseband=False)
 
     X4M300.clear_xep_buffer()
     counter = 0
     while 1:
         frame = X4M300.read_frame()
         if frame is not None:
-
             counter += 1
+            print(counter)
             print(frame)
             plt.plot(frame)
             plt.show()
-
