f1eb3fb1ab82ce1de3043900f9bac819e4f45590
Ziheng(Leo) Li <s-vector.lee@hotmail.com>
2020-08-23 01:48:56 -0400

    change xethrux4 worker to only emit when a frame is available

--- mGesf/workers.py
+++ mGesf/workers.py
@@ -293,6 +293,8 @@ class Xe4ThruWorker(QObject):
                                  'clutter_removal_frame': clutter_removal_frame,
                                  'clutter_removal_baseband_frame': clutter_removal_baseband_frame,
                                  'ir_spectrogram': np.array(list(ir_spectrogram))}
+                    # notify the uwb data for the sensor tab; only emit when a frame is available
+                    self.signal_data.emit(data_dict)
             else:
                 frame = sim_xe4thru()
                 self.frame_buffer.append(frame)
@@ -302,8 +304,9 @@ class Xe4ThruWorker(QObject):
                              'clutter_removal_frame': frame,
                              'clutter_removal_baseband_frame': frame,
                              'ir_spectrogram': np.array(list(ir_spectrogram))}
+                self.signal_data.emit(data_dict)  # notify the uwb data for the sensor tab
 
-            self.signal_data.emit(data_dict)  # notify the uwb data for the sensor tab
+
 
 
     def start_sensor(self, device_name, min_range, max_range, center_frequency, fps, baseband):
