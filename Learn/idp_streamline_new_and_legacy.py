ac764b0ada13b418b47037cb8db377b8055558a5
ApocalyVec <s-vector.lee@hotmail.com>
2020-09-03 00:48:00 -0400

    add error workaround in idp streamline legacy

--- Learn/idp_streamline_new_and_legacy.py
+++ Learn/idp_streamline_new_and_legacy.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+from tensorflow.python.keras.models import load_model
 
 from Learn.data_in import resolve_points_per_sample
 from utils.data_utils import load_idp_new_and_legacy
@@ -13,10 +14,10 @@ from utils.learn_utils import make_model
 import tensorflow as tf
 
 # Allow memory growth for the GPU
-gpu_devices = tf.config.experimental.list_physical_devices('GPU')
-for device in gpu_devices: tf.config.experimental.set_memory_growth(device, True)
+# gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+# for device in gpu_devices: tf.config.experimental.set_memory_growth(device, True)
 
-batch_size = 16
+batch_size = 8
 
 idp_complete_classes = [
     'A', 'B', 'C', 'D', 'E',
@@ -36,7 +37,7 @@ sensor_sample_points_dict = dict(
 encoder = OneHotEncoder(categories='auto')
 encoder.fit(np.reshape(idp_complete_classes, (-1, 1)))
 
-X_mmw_rD, X_mmw_rA, Y = load_idp_new_and_legacy('E:/data/mGesf/090120_hw_cr0.8',
+X_mmw_rD, X_mmw_rA, Y = load_idp_new_and_legacy('D:/data/mgesf/090120_hw_cr0.8',
                                                 sensor_feature_dict=sensor_feature_dict,
                                                 complete_class=idp_complete_classes, encoder=encoder,
                                                 sensor_sample_points_dict=sensor_sample_points_dict)
@@ -49,20 +50,38 @@ X_mmw_rD_train, X_mmw_rD_test, Y_train, Y_test = train_test_split(X_mmw_rD, Y, t
 X_mmw_rA_train, X_mmw_rA_test, Y_train, Y_test = train_test_split(X_mmw_rA, Y, test_size=0.20, random_state=3,
                                                                   shuffle=True)
 
-model = make_model(classes=idp_complete_classes, points_per_sample=sensor_sample_points_dict['mmw'],
-                   channel_mode='channels_first', batch_size=batch_size)
+from pathlib import Path
+import os
+model_dir = 'D:/ResearchProjects/mGesf/models/'
+train_completed = False
+load_existing = True
 
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=500)
-mc = ModelCheckpoint(
-    'D:/ResearchProjects/mGesf/models/' + str(datetime.datetime.now()).replace(':', '-').replace(' ',
-                                                                          '_') + '.h5',
-    monitor='val_accuracy', mode='max', verbose=1, save_best_only=True)
+while not train_completed:
+    sorted_model_paths = sorted(Path(model_dir).iterdir(), key=os.path.getmtime)
+    model_path = sorted_model_paths[-1]  # load the latest model
+    if load_existing:
+        print('load model at ' + str(model_path))
+        model = load_model(str(model_path))
+    else:
+        model = make_model(classes=idp_complete_classes, points_per_sample=sensor_sample_points_dict['mmw'],
+                           channel_mode='channels_first', batch_size=batch_size)
 
-history = model.fit(([X_mmw_rD_train, X_mmw_rA_train]), Y_train,
-                    validation_data=([X_mmw_rD_test, X_mmw_rA_test], Y_test),
-                    epochs=50000,
-                    batch_size=batch_size, callbacks=[es, mc], verbose=1, )
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=500)
+    mc = ModelCheckpoint(
+        'D:/ResearchProjects/mGesf/models/' + str(datetime.datetime.now()).replace(':', '-').replace(' ',
+                                                                              '_') + '.h5',
+        monitor='val_accuracy', mode='max', verbose=1, save_best_only=True)
 
+    try:
+        history = model.fit(([X_mmw_rD_train, X_mmw_rA_train]), Y_train,
+                            validation_data=([X_mmw_rD_test, X_mmw_rA_test], Y_test),
+                            epochs=50000,
+                            batch_size=batch_size, callbacks=[es, mc], verbose=1, )
+        train_completed = True
+    except:
+        print('exception caught, clearing tf session and reloading model')
+        load_existing = True
+        tf.keras.backend.clear_session()
 '''
 
 '''
