a1210e3a8d776acba7e3d8c91b740719eaeee08f
HaowenWeiJohn <hwei@wpi.edu>
2021-05-01 17:19:17 -0400

    load data test

--- Learn/DeepLearning_final_Project.py
+++ Learn/DeepLearning_final_Project.py
@@ -11,12 +11,7 @@ from utils.learn_utils import make_model, make_model_simple
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 import matplotlib.pyplot as plt
-import tensorflow as tf
 
-# Allow memory growth for the GPU
-# physical_devices = tf.config.experimental.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(physical_devices[0], True)
-# tf.config.experimental.set_memory_growth(physical_devices[1], True)
 
 idp_complete_classes = [
                         'A', 'B', 'C', 'D', 'E',
@@ -56,38 +51,4 @@ X_mmw_rD_train, X_mmw_rD_test, Y_train, Y_test = train_test_split(X_mmw_rD, Y, t
                                                                   shuffle=True)
 X_mmw_rA_train, X_mmw_rA_test, Y_train, Y_test = train_test_split(X_mmw_rA, Y, test_size=0.20, random_state=3,
                                                                   shuffle=True)
-
-#####################################################################################
-model = load_model('D:/PcProjects/mGesf/models/2020-09-02_22-14-36.768957.h5')
-# model = make_model(classes=idp_complete_classes, points_per_sample=sensor_sample_points_dict['mmw'])
-
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=500)
-mc = ModelCheckpoint(
-    '../models/' + str(datetime.datetime.now()).replace(':', '-').replace(' ',
-                                                                          '_') + '.h5',
-    monitor='val_accuracy', mode='max', verbose=1, save_best_only=True)
-
-history = model.fit(([X_mmw_rD_train, X_mmw_rA_train]), Y_train,
-                    validation_data=([X_mmw_rD_test, X_mmw_rA_test], Y_test),
-                    epochs=50000,
-                    batch_size=32, callbacks=[es, mc], verbose=1, )
-
-'''
-
-'''
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-
-# summarize history for loss
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+print("John")
